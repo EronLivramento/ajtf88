aafc1a16747115cf0d1c61633c448a3b3202f207
vinny <vinny@voffice.com.br>
2015-02-07 16:24:01 -0200

    Utilize o controle de fluxo condicional switch

--- labs/vinny/labs.aj1/ControleFluxoCondicional.java
+++ labs/vinny/labs.aj1/ControleFluxoCondicional.java
@@ -14,22 +14,49 @@ class ControleFluxoCondicional {
 	String nome = "Carolina Pereira";
 	int idade = 20;
 	String rg = "34.654.789-9";
-	char sexo = 'F';
+	char sexo = 'X';
 	double salario = 1500.0;
 	int anoAtual = 2009;
 	double aliquota = 0.20;
 	int anoNascimento = anoAtual - idade;
 	String sexoPorExtenso;
-	if (sexo == 'M') 
-		sexoPorExtenso = "masculino";
-	else 
-		sexoPorExtenso = "feminino";
+	String artigo;
+	String tratamento;
+	String nascido;
+	String portador;
+	String registrado;
+	switch (sexo){
+		case 'M': 
+			sexoPorExtenso = "masculino";
+			artigo = "O";
+			tratamento = " Senhor";
+			nascido = "nascido";
+			portador = "portador";
+			registrado = "registrado";
+			break;
+		case 'F': 
+			sexoPorExtenso = "feminino";
+			artigo = "A";
+			tratamento = " Senhora";
+			nascido = "nascida";
+			portador = "portadora";
+			registrado = "registrada";
+			break;
+		default:	
+			sexoPorExtenso = "indefinido";
+			artigo = "O(A)";
+			tratamento = " Senhor(a)";
+			nascido = "nascido(a)";
+			portador = "portador(a)";
+			registrado = "registrado(a)";
+			break;
+	}	
 	double impostoDevido = salario * aliquota;
-	String textNome = "O(a) Senhor(a) " + nome;
-	String textNasc = ", nascido(a) no ano de " + anoNascimento;
-	String textRg = " portador(a)do rg de numero " + rg + ", \n";
+	String textNome = artigo + tratamento + " " + nome;
+	String textNasc = ", " + nascido + " no ano de " + anoNascimento;
+	String textRg = " "+portador + " do rg de numero " + rg + ", \n";
 	String textSexo = " do sexo " + sexoPorExtenso;
-	String textTrabalho = ", está registrado(a) com o salário de R$ " + salario;
+	String textTrabalho = ", está " +registrado + " com o salário de R$ " + salario;
 	String textImposto = " e pagará R$ " + impostoDevido + " de imposto.";
 	System.out.println(textNome + textRg + textNasc + textSexo + textTrabalho
 	    + textImposto);
